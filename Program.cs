2a0fdfc82d0fe62a940029a4e6a33a08a0087c0a
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-19 19:31:37 +0530

    added Quotient and Remainder Program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            PowerOfTwo.PowerOfTheTwo();
+            QuotientRemainder.CalculateQutientRemainder();
         }
     }
 }
---
+++ QuotientRemainder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class QuotientRemainder
+    {
+        public static void CalculateQutientRemainder()
+        {
+            Console.WriteLine("Enter a number1 : ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a number2 : ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int quotient = number1 / number2;
+            int remainder = number1 % number2;
+
+            Console.WriteLine("Quoient : " + quotient);
+            Console.WriteLine("Remainder : " + remainder);
+        }
+    }
+}
