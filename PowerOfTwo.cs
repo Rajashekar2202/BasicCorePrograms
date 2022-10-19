75006cca67446fd1420f2884bd3dea53a32dabad
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-19 19:29:13 +0530

    added Power Of Two Program

---
+++ PowerOfTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    public class PowerOfTwo
+    {
+        public static void PowerOfTheTwo()
+        {
+            Console.WriteLine("Enter the Number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if(number >= 0 && number <= 31)
+            {
+                for(int i = 0; i < number; i++)
+                {
+                    Console.WriteLine(Math.Pow(2, i));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter the Number between 2 to 31 only");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            LeapYear.CheckLeapYear();
+            PowerOfTwo.PowerOfTheTwo();
         }
     }
 }
