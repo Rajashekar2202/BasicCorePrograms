a5c0c6c7b9f5a36f905fc54e93756ca3f7126d55
Rajashekar Vemula <vemula.rajashekar02@gmail.com>
2022-10-19 19:06:15 +0530

    added Even or Odd Program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Basic Core Programs");
+            EvenOdd.EvenOrOdd();
+           
         }
     }
 }
